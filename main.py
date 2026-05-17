3755e9a3231109558463d8dc42c4c7070ab1c8df
Neto test <kontur.prod@gmail.com>
2026-05-17 17:24:34 +0900

    Homework 3

--- main.py
+++ main.py
@@ -15,6 +15,25 @@ class Student:
                 lecturer.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def average_grade(self):
+        all_grades = []
+        for grade in self.grades.values():
+            all_grades += [grade]
+
+        if len(all_grades) == 0:
+            return 0
+
+        return sum(all_grades) / len(all_grades)
+
+    def __str__(self):
+        return (
+            f'Имя: {self.name}\n'
+            f'Фамилия: {self.surname}\n'
+            f'Средняя оценка за домашние задания: {self.average_grade()}\n'
+            f'Курсы в процессе изучения: {", ".join(self.courses_in_progress)}\n'
+            f'Завершенные курсы: {", ".join(self.finished_courses)}'
+        )
         
 class Mentor:
     def __init__(self, name, surname):
@@ -29,11 +48,26 @@ class Lecturer(Mentor):
         self.courses_attached = []
         self.grades = {}
 
+    def average_grade(self):
+        all_grades = []
+        for grade in self.grades.values():
+            all_grades += [grade]
+
+        if len(all_grades) == 0:
+            return 0
+
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка: {self.average_grade()}'
+
 class Reviewer(Mentor):
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
+
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}'
+
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -60,13 +94,18 @@ lecturer = Lecturer('Иван', 'Иванов')
 reviewer = Reviewer('Пётр', 'Петров')
 student = Student('Алёхина', 'Ольга', 'Ж')
 
+
 student.courses_in_progress += ['Python', 'Java']
 lecturer.courses_attached += ['Python', 'C++']
 reviewer.courses_attached += ['Python', 'C++']
 
-print(student.rate_lecture(lecturer, 'Python', 7))  # None
-print(student.rate_lecture(lecturer, 'Java', 8))  # Ошибка
-print(student.rate_lecture(lecturer, 'С++', 8))  # Ошибка
-print(student.rate_lecture(reviewer, 'Python', 6))  # Ошибка
+# print(student.rate_lecture(lecturer, 'Python', 7))  # None
+# print(student.rate_lecture(lecturer, 'Java', 8))  # Ошибка
+# print(student.rate_lecture(lecturer, 'С++', 8))  # Ошибка
+# print(student.rate_lecture(reviewer, 'Python', 6))  # Ошибка
+#
+# print(lecturer.grades)  # {'Python': [7]}
 
-print(lecturer.grades)  # {'Python': [7]}
+print(reviewer)
+print(lecturer)
+print(student)
